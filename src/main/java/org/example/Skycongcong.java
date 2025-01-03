d6ca0bc390ea419ae938669dd9756e3e4f7a8c74
minsekim1 <tkarnrwl7862@naver.com>
2025-01-03 10:35:25 +0900

    feat: add abstact Vehicle and class Skycongcong

---
+++ src/main/java/org/example/Skycongcong.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Skycongcong extends Vehicle {
+    public Skycongcong(String name) { super(name); }
+
+    public void move() {
+        System.out.println("Skycongcong move");
+    }
+
+    public void stop(){
+        System.out.println("Skycongcong stop");
+    }
+}
---
+++ src/main/java/org/example/Vehicle.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public abstract class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+    public abstract void move();
+    public abstract void stop();
+}
