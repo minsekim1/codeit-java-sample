b38cc4f784f2ec33cbecec0184f65574fc653c70
minsekim1 <tkarnrwl7862@naver.com>
2025-01-03 09:48:18 +0900

    feat: add Car class

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayInfo(){
+        System.out.println("Brand: " + brand + " Model: " + model + " Year: " + year);
+    }
+}
