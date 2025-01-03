1d0e2e7255bfb15d0b6604cb14d45a55f5ee3085
minsekim1 <tkarnrwl7862@naver.com>
2025-01-03 13:34:02 +0900

    feat: change main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        User u1 = new User("asd", "asdasd", "emial@naver.com");
+        User u1 = new User("asd", "asd", "emial@naver.com");
         u1.printUserInfo();
 
     }
