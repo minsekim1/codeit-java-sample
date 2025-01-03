64d7ae8a1c38f1dcc40696fd1d976e56fb736556
minsekim1 <tkarnrwl7862@naver.com>
2025-01-03 14:25:22 +0900

    feat: change Student field

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -2,10 +2,15 @@ package org.example;
 
 public class Student {
     private String name;
-    private int age;
+    private int score;
 
-    public Student(String name, int age) {
+    public Student(String name, int score) {
         this.name = name;
-        this.age = age;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", score=" + score + "]";
     }
 }
