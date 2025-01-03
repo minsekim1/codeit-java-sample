093cd15dc4b4dec5f6457707fb7b09b1eac7fc89
minsekim1 <tkarnrwl7862@naver.com>
2025-01-03 11:17:30 +0900

    feat: add class BankAccount

---
+++ src/main/java/org/example/BackAccount.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class BackAccount {
+    private double balance;
+
+    public BackAccount(double balance) {
+        if(balance > 0){
+            this.balance = balance;
+        }
+    }
+
+    public void deposit(double amount){
+        if(amount > 0){
+            this.balance += amount;
+            System.out.println("Deposited " + amount + " to " + this.balance);
+        }
+    }
+
+    public void withdraw(double amount){
+        if(amount > 0 && amount <= this.balance){
+            balance -= amount;
+            System.out.println("Withdrawn " + amount + " to " + this.balance);
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+}
